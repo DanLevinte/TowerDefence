d66c333ea1a2116a63633e784d667ef4aa0e312d
DanLevinte <96499270+DanLevinte@users.noreply.github.com>
2023-09-16 14:52:02 +0300

    f

--- Assets/Sword.cs
+++ Assets/Sword.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class Sword : MonoBehaviour
 {
     public int damage;
+    public bool isGivingDamage = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("DownEnemy")) { other.GetComponent<EnemyManager>().currentHealth -= damage; }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("DownEnemy"))
+        {
+            other.GetComponent<EnemyManager>().currentHealth -= damage;
+            other.GetComponent<EnemyManager>().switchColor = true;
+            other.GetComponent<MeshRenderer>().materials[0].color = Color.red;
+            isGivingDamage = true;
+        }
     }
 }
