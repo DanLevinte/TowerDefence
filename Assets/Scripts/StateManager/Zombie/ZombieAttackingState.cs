78e2e2afd3787512f38d7074831680ff68ea5ca9
DanLevinte <96499270+DanLevinte@users.noreply.github.com>
2023-09-21 22:30:58 +0300

    rot to target

--- Assets/Scripts/StateManager/Zombie/ZombieAttackingState.cs
+++ Assets/Scripts/StateManager/Zombie/ZombieAttackingState.cs
@@ -8,9 +8,12 @@ public class ZombieAttackingState : State
 
     public MobManager mobManager;
 
+    public LayerMask targetMask;
+
     public override State RunCurrentState()
     {
-        //if (mobManager.target == null) { return this.movingState; }
+        if (mobManager.target != null) { RecheckTarget(); }
+        else { return this.movingState; }
 
         return this;
     }
@@ -18,5 +21,23 @@ public class ZombieAttackingState : State
     public override string GetStateName()
     {
         return "zombie_attack";
+    }
+
+    private void RecheckTarget()
+    {
+        var detections = Physics.OverlapSphere(mobManager.transform.position, 1, targetMask);
+        
+        if (detections.Length != 0)
+        {
+            for (int i = 0; i <= detections.Length - 1 ; i++)
+            {
+                if (mobManager.target == detections[i].gameObject) 
+                {
+                    Vector3 pos = new Vector3(detections[i].transform.position.x, this.gameObject.transform.position.y, detections[i].transform.position.z);
+                    this.mobManager.mob.transform.LookAt(pos);
+                    break;
+                }
+            }
+        }
     }
 }
